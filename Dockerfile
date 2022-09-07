4fe76d81fb785c237752ab760d8233d5754c0a77
Koushik Roy <koushikroy@Koushiks-MacBook-Air.local>
2022-09-07 20:46:30 +0530

    install requirements

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,3 @@
-# FROM python:3.7.2
-
-# WORKDIR /usr/src/app
-# RUN pip install pipreqs
-# RUN pipreqs . --force
-# # COPY requirements.txt ./
-# RUN python -m pip install --upgrade pip
-# RUN pip install --no-cache-dir -r requirements.txt
-
-# COPY . .
-# EXPOSE 5000
-# CMD [ "python3", "./index.py" ]
-
-
 FROM python:3.7.2
 
 # Create app directory 
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# read requirements.txt from modules directory and echo the module names
+# to stdout
+custom_req=$(cat modules/requirements.txt | grep -v '^#' | grep -v '^$' | cut -d'=' -f1)
+
+# read requirements.txt
+# for each line, echo the module name to stdout
+auto_req=$(cat requirements.txt | grep -v '^#' | grep -v '^$' | cut -d'=' -f1)
+
+# write custom_req auto_req to a file
+echo "$custom_req $auto_req" > requirements.txt
+
