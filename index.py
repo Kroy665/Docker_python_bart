9ff85c49bae6d17b6eba9ab002af7f33cc72ea2c
Koushik Roy <koushikroy@Koushiks-MacBook-Air.local>
2022-09-07 17:31:14 +0530

    file cleaned

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 import sys
 sys.path.insert(1, 'modules')
 from flask_cors import CORS
-from createSummarizer import createSummarizer
 from modules.getModules_app import *
 
 
@@ -11,40 +10,7 @@ from modules.getModules_app import *
 app = Flask(__name__)
 CORS(app)
 
-print("Testing")
-
-
-mod = getModules()
-print("getModules()::",mod)
-
-
 endpoint = '/api/v1'
-
-# http://127.0.0.1:5000/api/v1/create-summarize
-@app.route(endpoint + '/create-summarize', methods=['GET','POST'])
-def get_function():
-    if request.method == 'GET': 
-
-        print(request)
-        
-        # whatever you wanne do
-        s = {"value": "data"}
-        return jsonify(s), 200
-
-    
-    if request.method == 'POST': 
-        request_data = request.get_json()
-        print(request_data)
-        
-        # whatever you wanne do
-        if request_data['text']:
-            res = createSummarizer(request_data['text'])
-
-
-        s = {"data": res}
-        return jsonify(s), 200
-
-
 
 allFunction = []
 for module in getModules():
@@ -55,7 +21,7 @@ for module in getModules():
             # print(func[1].__name__)
             allFunction.append(func)
 
-
+# http://localhost:6000/api/v1/custom-function?routeCalled={Function_name}&prompt={prompt}
 @app.route(endpoint + '/custom-function', methods=['GET','POST'])
 def customFunction():
     if request.method == 'POST':
@@ -66,15 +32,9 @@ def customFunction():
             if func[0] == routeCalled:
                 print(func[1])
                 return func[1](prompt)
-
-        # return jsonify({"data": "data"}), 200
-
-            
-
-
-
-            
-
+    else:
+        return "Invalid request"
+        
 # Start app on http://127.0.0.1:5000/api/v1
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=6000, debug=True)
