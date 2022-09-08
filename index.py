5fa5acb5a0e794d5181238da336065d65055ebe5
Koushik Roy <koushikroy@Koushiks-MacBook-Air.local>
2022-09-08 22:19:43 +0530

    allModules.txt added

--- index.py
+++ index.py
@@ -76,4 +76,4 @@ def customFunction():
         
 # Start app on http://127.0.0.1:5000/api/v1
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
